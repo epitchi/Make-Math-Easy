2ef982c7582bd60aeec27d7da6a6f1ea7a2f34be
ThienejLee <thienvanlea1@gmail.com>
2020-10-07 23:32:48 +0700

    add inputdecoration

--- lib/screen/firstModePage.dart
+++ lib/screen/firstModePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:solveMathApp/widgets/widget.dart';
 
 class FirstModePage extends StatefulWidget {
   @override
@@ -6,42 +7,95 @@ class FirstModePage extends StatefulWidget {
 }
 
 class _FirstModePageState extends State<FirstModePage> {
+  TextEditingController xTextEditingController = new TextEditingController();
+  TextEditingController yTextEditingController = new TextEditingController();
+  TextEditingController zTextEditingController = new TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  String validateMobile(String value) {
+    String pattern = r'(^(?:[+0]9)?[0-9]{10,12}$)';
+    RegExp regExp = new RegExp(pattern);
+    if (value.length == 0) {
+      return 'Please enter number';
+    } else if (!regExp.hasMatch(value)) {
+      return 'Please enter valid mobile number';
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("Mode 1"),
         ),
-        body: Container(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(hintText: "Điền x"),
-                  onChanged: (value) {
-                    setState(() {
-                      //num1 == num parse .
-                    });
-                  },
-                ),
-                TextFormField(
-                  decoration: InputDecoration(hintText: "Điền y"),
-                  onChanged: (value) {
-                    setState(() {
-                      //num1 == num parse .
-                    });
-                  },
-                ),
-                TextFormField(
-                  decoration: InputDecoration(hintText: "Điền z"),
-                  onChanged: (value) {
-                    setState(() {
-                      //num1 == num parse .
-                    });
-                  },
-                ),
-              ],
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height -5,
+            // alignment: Alignment.bottomCenter,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Form(
+                    key: formKey,
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          controller: xTextEditingController,
+                          decoration: textFieldInputDecoration("Điền X"),
+                          validator: (val) {
+                            validateMobile(val);
+                          },
+                        ),
+                        TextFormField(
+                          controller: yTextEditingController,
+                          decoration: textFieldInputDecoration("Điền Y"),
+                          validator: (val) {
+                            validateMobile(val);
+                          },
+                          onChanged: (value) {
+                            setState(() {
+                              //num1 == num parse .
+                            });
+                          },
+                        ),
+                        TextFormField(
+                          controller: zTextEditingController,
+                          decoration: textFieldInputDecoration("Điền Z"),
+                          validator: (val) {
+                            validateMobile(val);
+                          },
+                          onChanged: (value) {
+                            setState(() {
+                              //num1 == num parse .
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      RaisedButton(
+                        onPressed: () {
+                          //solve, wait ML nqu
+                        },
+                        child: Text("Solve"),
+                      ),
+                      RaisedButton(
+                        onPressed: () {
+                          xTextEditingController.text = "";
+                          yTextEditingController.text = "";
+                          zTextEditingController.text = "";
+                        },
+                        child: Text("Delete"),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ));
---
+++ lib/widgets/widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+InputDecoration textFieldInputDecoration(String hintText) {
+  return InputDecoration(
+      hintText: hintText,
+      hintStyle: TextStyle(color: Colors.black26),
+      focusedBorder: UnderlineInputBorder(
+        borderSide: BorderSide(color: Colors.black),
+      ),
+      enabledBorder: UnderlineInputBorder(
+        borderSide: BorderSide(color: Colors.black),
+      ));
+}
+
+TextStyle simpleTextStyle() {
+  return TextStyle(
+    color: Colors.white,
+    fontSize: 16,
+  );
+}
+
+TextStyle mediumTextStyle() {
+  return TextStyle(color: Colors.white, fontSize: 17);
+}
