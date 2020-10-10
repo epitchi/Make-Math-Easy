537f0364c87c84ef417cc1715dcdf3a74cb0a49b
ThienejLee <thienvanlea1@gmail.com>
2020-10-10 08:03:08 +0700

    add icon

--- lib/main.dart
+++ lib/main.dart
@@ -41,23 +41,60 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Padding(
           padding: EdgeInsets.all(5),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              ListTile(
-                title: Text("Giải toán"),
+              InkWell(
                 onTap: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => MenuPage()),
                   );
                 },
+                child: Padding(
+                    padding: EdgeInsets.all(5),
+                    child: ListTile(
+                      title: Text(
+                        "Giải Toán",
+                        style: TextStyle(
+                            fontSize: 32,
+                            backgroundColor: Colors.blue,
+                            color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                      subtitle: Image.asset(
+                        "assets/images/img_math_solve.png",
+                        width: 200,
+                        height: 200,
+                      ),
+                    )),
               ),
-              ListTile(
-                title: Text("Giới thiệu"),
+
+              InkWell(
                 onTap: () {
                   Navigator.push(context,
                       MaterialPageRoute(builder: (context) => IntroducePage()));
                 },
-              )
+                child: Padding(
+                    padding: EdgeInsets.all(5),
+                    child: ListTile(
+                      title: Text(
+                        "Giới Thiệu",
+                        style: TextStyle(
+                            fontSize: 32,
+                            backgroundColor: Colors.blue,
+                            color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                      subtitle: Image.asset(
+                        "assets/images/img_info.png",
+                        width: 200,
+                        height: 200,
+                      ),
+                    )),
+              ),
+              // Image.asset(""),
+              // ListTile(
+              //   title: Text("Giới thiệu"),
             ],
           ),
         ),
