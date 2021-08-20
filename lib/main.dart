c7db4f13eec7b40139cf0864ccfc57decf34f194
epitchi <thienvanlea1@gmail.com>
2021-08-20 10:30:57 +0700

    fix responsize on mobile

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 children: [
                   NavigationBar(),
                   Dashboard(),
-                  CalenderSpace(),
+                  ResponsiveLayout.isMacbook(context) ? CalenderSpace() : Container(),
                 ],
               ) 
               // : ListView(
